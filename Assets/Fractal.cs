79d9372d0fc390e1f7662d24859d5cbfecc5924b
Ajinkya Kulkarni <quaintquid@gmail.com>
2015-05-30 12:43:16 +0530

    added more colours, rotation, random spawning

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class Fractal : MonoBehaviour {
 
-	public Mesh mesh;
+	public Mesh[] meshes;
 	public Material material;
-	private Material[] materials;
+	private Material[,] materials;
 
 	public int maxDepth;
 	private int depth;
+	public float spawnProbability;
 	// Use this for initialization
 
 		
@@ -16,8 +17,8 @@ public class Fractal : MonoBehaviour {
 		if (materials == null) {
 			InitializeMaterial(); 
 		}
-		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
-		gameObject.AddComponent<MeshRenderer> ().material = materials [depth];
+		gameObject.AddComponent<MeshFilter> ().mesh = meshes[Random.Range(0, meshes.Length)];
+		gameObject.AddComponent<MeshRenderer> ().material = materials [depth, Random.Range (0,2)];
 		if (depth < maxDepth)
 		{
 			StartCoroutine(CreateChildren());
@@ -25,19 +26,23 @@ public class Fractal : MonoBehaviour {
 	}
 
 	 private void InitializeMaterial(){
-		materials = new Material[maxDepth + 1];
+		materials = new Material[maxDepth + 1, 2];
 		for (int i=0; i<=maxDepth; i++) {
-			materials[i] = new Material(material);
-			materials[i].color = Color.Lerp (Color.white, Color.yellow, (float)i / maxDepth);
+			materials[i, 0] = new Material(material);
+			materials[i, 0].color = Color.Lerp (Color.white, Color.yellow, (float)i / maxDepth);
+			materials[i, 1] = new Material(material);
+			materials[i, 1].color = Color.Lerp (Color.white, Color.cyan, (float)i / maxDepth);
 	
 		}
-			
+		materials [maxDepth, 0].color = Color.red;
+		materials [maxDepth, 1].color = Color.magenta;
 	}
 
 	public float childScale; // scale for shrinking the next layer (default set to 0.5)
 
 	private void Initialize (Fractal parent, int childIndex){
-		mesh = parent.mesh; 
+		spawnProbability = parent.spawnProbability;
+		meshes = parent.meshes; 
 		materials = parent.materials;
 		maxDepth = parent.maxDepth; //conserving parent variables
 		transform.localRotation = orientation[childIndex]; //rotating current object by 'orientation' degrees in all three planes
@@ -50,13 +55,18 @@ public class Fractal : MonoBehaviour {
 
 	private IEnumerator CreateChildren () {
 		for (int i=0; i<childDirections.Length; i++) {
-		yield return new WaitForSeconds(0.5f);
+			if (Random.value < spawnProbability){
+		yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
 		new GameObject("Fractal Child ").
 			AddComponent<Fractal>().Initialize(this, i);
+		}
 		}
 	}
 		
 	// Update is called once per frame
+	private void Update () {
+		transform.Rotate(15f * Time.deltaTime, 15f * Time.deltaTime, 0f);
+	}
 
 	private static Vector3[] childDirections = {
 		Vector3.up,
